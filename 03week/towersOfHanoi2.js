2fdad7a9415957622d583312a2169951d10a5042
Eric Garcia <egarcia1868@gmail.com>
2017-07-03 19:33:01 -0500

    started/finished ToH

--- 03week/towersOfHanoi2.js
+++ 03week/towersOfHanoi2.js
@@ -19,24 +19,37 @@ function printStacks() {
   console.log("c: " + stacks.c);
 }
 
-function movePiece() {
+function movePiece(startStack, endStack) {
   // Your code here
+  stacks[endStack].push(stacks[startStack].pop());
 
 }
 
-function isLegal() {
+function isLegal(startStack, endStack) {
   // Your code here
-
+  if (stacks[startStack][stacks[startStack].length-1] < stacks[endStack][stacks[endStack].length-1]
+     || stacks[endStack].length ===0) {
+    return true;
+  } else {
+    console.log
+    return false;
+  }
 }
 
 function checkForWin() {
   // Your code here
-
+  if (stacks.b.length === 4 || stacks.c.length === 4) {
+    return true;
+  } else{
+    return false;
+  }
 }
 
 function towersOfHanoi(startStack, endStack) {
   // Your code here
-
+  if (isLegal(startStack, endStack)) {
+    movePiece(startStack, endStack);
+  }
 }
 
 function getPrompt() {
@@ -44,7 +57,11 @@ function getPrompt() {
   rl.question('start stack: ', (startStack) => {
     rl.question('end stack: ', (endStack) => {
       towersOfHanoi(startStack, endStack);
+      if (checkForWin()) {
+        console.log("Congratulations!  You're not a complete idiot!")
+      } else {
       getPrompt();
+    }
     });
   });
 }
@@ -88,6 +105,6 @@ if (typeof describe === 'function') {
   });
 } else {
 
-  getPrompt();
+getPrompt();
 
 }
